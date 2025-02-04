7ae6656bc7f2a66bb6cbfecfd3c5b91bb84e3348
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-02-04 10:05:23 +0900

    [Bronze II] Title: 소수 찾기, Time: 92 ms, Memory: 9416 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.js
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.js
@@ -3,7 +3,7 @@ const N = +arr[0];
 const nums = arr[1].split(" ").map(Number);
 const ans = nums.filter(num => {
     if(num === 1) return false;
-    for(let i=2; i<num; i++) {
+    for(let i=2; i<=num/2; i++) {
         if(num % i === 0) {
             return false;
         }
