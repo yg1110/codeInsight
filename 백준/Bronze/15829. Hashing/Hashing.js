71856d2872709126ef4578e1f058ae61b9028cbc
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-02-14 10:08:26 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9344 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.js
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -1,8 +1,10 @@
 const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const word = arr[1].split("");
-const sum = word.reduce((acc, curr, index) => {
-    const alpha = curr.charCodeAt(0) - 96;
-    const hash = Math.pow(31, index);
-    return acc + (alpha * hash);
-}, 0);
-console.log(sum);
+const word = arr[1];
+const MOD = 1234567891n;
+let sum = 0n;
+for(let i=0; i<word.length; i++) {
+    const alpha = BigInt(word[i].charCodeAt(0) - 96);
+    const hash = (31n ** BigInt(i)) % MOD;
+    sum = (sum + (alpha * hash) % MOD) % MOD;
+}
+console.log(sum.toString());
