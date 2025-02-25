f40d2dc4eb02c13746ad92981956d3b83a606672
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-02-25 17:33:33 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 412 ms, Memory: 58292 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.js
@@ -1,13 +1,14 @@
 const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 const N = +arr.shift();
-const coord = arr.map(item => 
-    item.split(" ")
-    .map(Number))
+const coords = arr.map(
+    item => item.split(" ").map(Number))
     .sort((a, b) => {
         if(a[0] === b[0]) return a[1] - b[1];
         return a[0] - b[0]
-    }
-);
+    });
+
+let answer = '';
 for(let i=0; i<N; i++) {
-    console.log(`${coord[i][0]} ${coord[i][1]}`);
+    answer += `${coords[i][0]} ${coords[i][1]}\n`;
 }
+console.log(answer);
