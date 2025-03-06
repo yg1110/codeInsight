3944fe9305c319b507364d241ea01397e6476ec5
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-03-06 21:55:35 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 3664 ms, Memory: 85680 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M] = input.shift().split(" ").map(Number);
+const pokemons = {};
+const questions = input.slice(N, N + M);
+for(let i=1; i<=N; i++) {
+    const pokemon = input[i - 1];
+    pokemons[i] = pokemon;
+    pokemons[pokemon] = i;
+}
+for(let i=0; i<M; i++) {
+    const key = input[N + i];
+    const pokemon = pokemons[key];
+    console.log(pokemon);
+}
