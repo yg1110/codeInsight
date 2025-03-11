df1d5d452750ae71502358a38211cb7761e6e373
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-03-11 13:42:42 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 444 ms, Memory: 71576 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.js
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.js
@@ -1,11 +1,16 @@
 const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
 const [N, M] = arr.shift().split(" ").map(Number);
 const memo = {};
+let result = "";
+
 for(let i=0; i<N; i++) {
     const [site, password] = arr[i].split(" ");
     memo[site] = password;
 }
+
 for(let i=N; i<N+M; i++) {
     const site = arr[i];
-    console.log(memo[site]);
+    result += memo[site] + '\n';
 }
+
+console.log(result);
