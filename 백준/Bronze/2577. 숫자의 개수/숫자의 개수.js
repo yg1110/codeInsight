979e92fa5e69b0391e6833c2842c153edee42910
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-12-18 15:42:10 +0900

    [Bronze II] Title: 숫자의 개수, Time: 88 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -1,18 +1,17 @@
-const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
-const nums = (arr[0] * arr[1] * arr[2]).toString().split("");
-const ans = {
-    0: 0,
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
-    7: 0,
-    8: 0,
-    9: 0
-};
-nums.forEach(num => {
-    ans[num] += 1;
-});
-console.log(Object.values(ans).join("\n"));
+const [A, B, C] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+const nums = (A * B * C).toString().split("").map(Number);
+const num = {};
+for (let i = 0; i < 10; i++) {
+  num[i] = 0;
+}
+for (let i = 0; i < nums.length; i++) {
+  num[nums[i]]++;
+}
+
+console.log(Object.values(num).join("\n"));
