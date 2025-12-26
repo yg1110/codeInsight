a65e719b93820df16893843b60d6dc28da294165
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-12-26 17:10:45 +0900

    [Silver V] Title: 문서 검색, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.js
+++ 백준/Silver/1543. 문서 검색/문서 검색.js
@@ -1,10 +1,8 @@
-const [doc, target] = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-let count = 0;
-for(let i=0; i<doc.length - target.length + 1; i++) {
-    const current = doc.slice(i, i + target.length);
-    if(current === target) {
-        count++;
-        i += target.length - 1;
-    }
-}
-console.log(count);
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const reg = new RegExp(input[1], "gi");
+const rest = input[0].replaceAll(reg, "");
+console.log((input[0].length - rest.length) / input[1].length);
