b215ac13db9f49b4931681b0024c9d207e6568d4
yg1110 <34371366+yg1110@users.noreply.github.com>
2025-12-26 17:40:17 +0900

    [Silver V] Title: 방 번호, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.js
+++ 백준/Silver/1475. 방 번호/방 번호.js
@@ -1,7 +1,21 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
-const nums = new Array(9).fill(0);
-input.split("").forEach((num) => {
-  if (num === "6" || num === "9") nums[6] += 0.5;
-  else nums[num]++;
+const nums = input.split("").map((n) => (n === "6" || n === "9" ? 6 : +n));
+const numsObj = {
+  0: 0,
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+};
+let max = 0;
+nums.forEach((num) => {
+  numsObj[num]++;
 });
-console.log(Math.ceil(Math.max(...nums)));
+Object.entries(numsObj).forEach(([key, value]) => {
+  max = Math.max(max, key === "6" ? Math.ceil(value / 2) : value);
+});
+console.log(max);
