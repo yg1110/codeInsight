98da647bb63ef6c6a8607d16a72157756b3d40ef
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-01-06 17:03:50 +0900

    [Silver IV] Title: 괄호, Time: 96 ms, Memory: 9488 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.js
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -1,18 +1,27 @@
-const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-for(let i=1; i<arr.length; i++) {
-    const bracket = arr[i].split("");
-    const stack = [];
-    let VPS = "YES";
-    for(let j=0; j<bracket.length; j++) {
-        if(bracket[j] === "(") stack.push(bracket[j]);
-        if(bracket[j] === ")") {
-            if(stack[stack.length - 1] !== "(") {
-                VPS = "NO";
-                break;
-            };
-            stack.pop();
-        }
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = +input[0];
+
+function verification(brackets) {
+  const stack = [];
+  for (const b of brackets) {
+    if (b === "(") {
+      stack.push(b);
+    } else {
+      const current = stack.pop();
+      if (current !== "(") return "NO";
     }
-    if(stack.length > 0) VPS = "NO";
-    console.log(VPS);
+  }
+  return stack.length === 0 ? "YES" : "NO";
 }
+
+let ans = "";
+for (let i = 1; i <= N; i++) {
+  const brackets = input[i].split("");
+  ans += verification(brackets) + "\n";
+}
+
+console.log(ans.trim());
