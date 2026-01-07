7c56dfb2d0d96060ac5e405a778bbac8a454e85a
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-01-07 18:04:43 +0900

    [Silver I] Title: 미로 탐색, Time: 168 ms, Memory: 12252 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -1,26 +1,55 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-const solution = () => {
-  const [n, m] = input[0].split(' ').map(Number);
-  const graph = input.slice(1).map((v) => v.split('').map(Number));
-  const visited = Array.from(Array(n), () => Array(m).fill(false));
-  const dx = [0, 0, 1, -1];
-  const dy = [1, -1, 0, 0];
-  const queue = [{ x: 0, y: 0 }];
-  while (queue.length) {
-    const { x, y } = queue.shift();
-    for (let i = 0; i < 4; i++) {
-      const nx = x + dx[i];
-      const ny = y + dy[i];
-      if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-      if (graph[nx][ny] === 0) continue;
-      if (visited[nx][ny]) continue;
-      visited[nx][ny] = true;
-      graph[nx][ny] = graph[x][y] + 1;
-      queue.push({ x: nx, y: ny });
+const [N, M] = input[0].split(" ").map(Number);
+const map = [];
+const visit = [];
+const ans = [];
+const di = [-1, 1, 0, 0];
+const dj = [0, 0, -1, 1];
+for (let i = 1; i <= N; i++) {
+  map.push(input[i].split(""));
+  visit.push(Array(M).fill(false));
+}
+
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < M; j++) {
+    if (map[i][j] === "1" && !visit[i][j]) {
+      bfs({ i, j, cnt: 1 });
     }
   }
-  return graph[n - 1][m - 1];
-};
+}
 
-console.log(solution());
+function bfs(node) {
+  const queue = [];
+  let idx = 0;
+  queue.push(node);
+
+  while (idx < queue.length) {
+    const current = queue[idx++];
+    for (let d = 0; d < 4; d++) {
+      const ni = current.i + di[d];
+      const nj = current.j + dj[d];
+      if (
+        ni >= 0 &&
+        ni < N &&
+        nj >= 0 &&
+        nj < M &&
+        !visit[ni][nj] &&
+        map[ni][nj] === "1"
+      ) {
+        visit[ni][nj] = true;
+        queue.push({ i: ni, j: nj, cnt: current.cnt + 1 });
+        if (ni === N - 1 && nj === M - 1) {
+          ans.push(current.cnt + 1);
+        }
+      }
+    }
+  }
+}
+
+ans.sort((a, b) => a - b);
+console.log(ans[0]);
