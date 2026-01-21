f4e297890c62e4bfb8d079e5bbdee95d2e7dcde6
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-01-21 15:09:51 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 792 ms, Memory: 149364 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -1,12 +1,17 @@
-const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-const N = +arr[0];
-const M = +arr[2];
-const nums = arr[1].split(" ");
-const picks = arr[3].split(" ");
-const numsObj = {};
-nums.forEach(num => {
-    if(numsObj[num]) numsObj[num]++;
-    else numsObj[num] = 1;
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const cards = input[1].split(" ");
+const target = input[3].split(" ");
+const nums = new Map();
+let ans = "";
+
+cards.forEach(c => {
+  const n = nums.get(c);
+  if(n) {
+    nums.set(c, n + 1);
+  } else {
+    nums.set(c, 1);
+  }
 });
-const result = picks.map(pick => numsObj[pick] || 0).join(" ");
-console.log(result);
+
+target.forEach(t => ans += (nums.get(t) || 0) + " ");
+console.log(ans.trim());
