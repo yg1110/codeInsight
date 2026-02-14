3bd87199582dddb1f22f98494864ebda4469ded0
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-02-14 09:19:34 +0900

    [Bronze III] Title: 크림빵, Time: 88 ms, Memory: 9336 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/28214. 크림빵/크림빵.js
@@ -0,0 +1,27 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [N, K, P] = input[0].split(" ").map(Number);
+const bread = input[1].split(" ");
+let ans = 0;
+
+function checkDefective(i) {
+  let cnt = 0;
+  for (let j = 0; j < K; j++) {
+    const index = N * i + j;
+    if (bread[index] === "0") {
+      cnt++;
+    }
+    if (cnt >= P) return false;
+  }
+  return true;
+}
+
+for (let i = 0; i < N; i++) {
+  ans += checkDefective(i) ? 1 : 0;
+}
+
+console.log(ans);
