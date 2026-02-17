2912fb8bef58cf27347d7fc37545135f5f99d700
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-02-18 08:40:01 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 96 ms, Memory: 9400 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.js
@@ -0,0 +1,8 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ");
+const a = BigInt(input[0]);
+const b = BigInt(input[1]);
+console.log((a + b).toString());
