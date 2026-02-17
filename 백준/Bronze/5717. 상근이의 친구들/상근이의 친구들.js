5b4a5604f4f472d5bbf503f5ae1607eecb796ebe
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-02-18 08:24:14 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 88 ms, Memory: 9440 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.js
@@ -0,0 +1,9 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+for (let i = 0; i < input.length - 1; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  console.log(a + b);
+}
