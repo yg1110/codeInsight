66ab815317f9924f9d0b7e6741691054161842cf
yg1110 <34371366+yg1110@users.noreply.github.com>
2026-03-19 22:20:25 +0900

    [Bronze I] Title: 딱지놀이, Time: 188 ms, Memory: 12692 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.js
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const lines = fs.readFileSync(0, "utf8").trim().split("\n");
 const N = +lines[0];
+let result = "";
 function play(a, b) {
-  const end = Math.max(a.cnt, b.cnt);
-  for (let i = end; i > 0; i--) {
+  for (let i = 4; i >= 1; i--) {
     if ((a[i] || 0) > (b[i] || 0)) return "A";
     if ((a[i] || 0) < (b[i] || 0)) return "B";
   }
@@ -12,21 +12,18 @@ function play(a, b) {
 for (let i = 1; i <= N * 2; i += 2) {
   const a = lines[i].split(" ").reduce((acc, cur, idx) => {
     if (idx > 0) {
-      acc[cur] = (acc[cur] || 0) + 1;
-      return acc;
-    } else {
-      acc["cnt"] = cur;
+      acc[+cur] = (acc[+cur] || 0) + 1;
       return acc;
     }
+    return acc;
   }, {});
   const b = lines[i + 1].split(" ").reduce((acc, cur, idx) => {
     if (idx > 0) {
-      acc[cur] = (acc[cur] || 0) + 1;
-      return acc;
-    } else {
-      acc["cnt"] = cur;
+      acc[+cur] = (acc[+cur] || 0) + 1;
       return acc;
     }
+    return acc;
   }, {});
-  console.log(play(a, b));
+  result += play(a, b) + "\n";
 }
+console.log(result.trim());
